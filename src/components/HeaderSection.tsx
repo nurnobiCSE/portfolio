73f75f8e77968ed2bb5af7136199566ad481a401
nurnobicse <shuvo517942@gmail.com>
2023-10-22 12:08:18 +0600

    feat: change home url2

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -9,7 +9,7 @@ const HeaderSection = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a href="/" className="hover:text-[#FFB800] font-ppns text-base">HOME</a></li>
+                            <li><a href="/portfolio/" className="hover:text-[#FFB800] font-ppns text-base">HOME</a></li>
                             <li><a href="#about" className="hover:text-[#FFB800] font-ppns text-base">ABOUT</a></li>
                             <li><a href="#portfolio" className="hover:text-[#FFB800] font-ppns text-base">PORTFOLIO</a></li>
                             <li><a href="#contact" className="hover:text-[#FFB800] font-ppns text-base">CONTACT</a></li>
